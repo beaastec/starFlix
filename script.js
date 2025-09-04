199c042a9c452a90dc4d45c0eca792cbbeccb8ff
Bea <beatriz.lima.silva03@escola.pr.gov.br>
2025-09-04 09:58:41 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('bnt.proximo');
+const reiniciarBtn = document.getElementById('btn-reiniciar');
+
+avanca.forEach(button =>
+    button.addEventListener('click')
+)
+    
